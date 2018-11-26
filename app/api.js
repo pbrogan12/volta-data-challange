9b3391d46c4e62008f5bdb255935dce9ca562426
Patrick Brogan <pbrogan12@gmail.com>
2018-11-26 13:31:34 -0500

    Init commit of api.  Includes api call to fetch stations

---
+++ app/api.js
@@ -0,0 +1,12 @@
+import { BASE_API_URL } from "config.js";
+
+export function getStations() {
+  return fetch(BASE_API_URL + "stations")
+    .then(response => response.json())
+    .then(responseJson => {
+      return responseJson;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
